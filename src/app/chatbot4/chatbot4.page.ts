12ee51e474859185983a423ce4654bfe3588f38c
KerryMac <kerrymac@KerryMacdeMacBook-Pro.local>
2020-01-10 12:00:32 +0800

    use scroll-padding-hack in echo bot

--- src/app/chatbot4/chatbot4.page.ts
+++ src/app/chatbot4/chatbot4.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Observable, of, Subscription, fromEvent } from 'rxjs';
+import { Platform, Config } from '@ionic/angular';
 
 declare let $: any;
 declare let kendo: any;
@@ -8,17 +10,71 @@ declare let kendo: any;
     templateUrl: 'chatbot4.page.html',
     styleUrls: ['chatbot4.page.scss']
 })
-export class Chatbot4Page implements OnInit, AfterViewInit {
+export class Chatbot4Page implements OnInit, AfterViewInit, OnDestroy {
+
     @ViewChild('chatDiv', { static: false }) chatDiv: ElementRef;
-    constructor() {
+    private keybaordWillShowSub: Subscription;
+    private keyboardWillHideSub: Subscription;
+    private keybaordDidShowSub: Subscription;
+
+    constructor(private platform: Platform, private config: Config) {
     }
 
     ngOnInit() {
-
     }
 
     ngAfterViewInit() {
         this.setupChat();
+        // 1.
+        // 2.
+        //this.addKeyboardListeners();
+        this.chatDiv.nativeElement.querySelector('input.k-input').dataset['no_ionic_scroll_padding']='true';
+    }
+
+    ngOnDestroy() {
+        // 1.
+        //this.removeKeyboardListeners();
+    }
+
+    private addKeyboardListeners() {
+        if (this.platform.is("cordova") && this.platform.is("ios")) {
+            this.keybaordWillShowSub = fromEvent(window, 'keyboardWillShow').subscribe(e => {
+                console.log(e);
+                // 1.
+                // const resizeHeight = 286 + (e as any).keyboardHeight;
+                // $(this.chatDiv.nativeElement).css({ "height": resizeHeight + 'px' });
+                // $(this.chatDiv.nativeElement).css({ "height": 'unset' });
+                // $(this.chatDiv.nativeElement).css({ "max-height": 'unset' });
+
+                // 2.
+                // replace with scroll-padding-hack
+                // this.chatDiv.nativeElement.closest('ion-content').style.setProperty("--keyboard-offset","0px")
+            });
+            this.keyboardWillHideSub = fromEvent(window, 'keyboardWillHide').subscribe(e => {
+                console.log(e);
+                // 1.
+                //$(this.chatDiv.nativeElement).css({ "height": '100%' });
+                //$(this.chatDiv.nativeElement).css({ "max-height": '100%' });
+            });
+
+            // make input scroll into view
+            // this.keybaordDidShowSub = fromEvent(window, 'keyboardDidShow').subscribe(e => {
+            //     console.log(e);
+            //     setTimeout(()=>{
+            //         const activeEle=document.activeElement;
+            //         console.log(activeEle);
+            //         if(activeEle.className=='k-input'){
+            //             activeEle.scrollIntoView();
+            //         }
+            //     },200);
+            // });
+        }
+    }
+
+    private removeKeyboardListeners() {
+        if (this.keybaordWillShowSub) this.keybaordWillShowSub.unsubscribe();
+        if (this.keyboardWillHideSub) this.keyboardWillHideSub.unsubscribe();
+        if (this.keybaordDidShowSub) this.keybaordDidShowSub.unsubscribe();
     }
 
     setupChat() {
@@ -36,6 +92,7 @@ export class Chatbot4Page implements OnInit, AfterViewInit {
             post: (args) => {
                 setTimeout(() => {
                     chat.renderMessage({ type: 'text', text: 'You just said: ' + args.text, timestamp: new Date() }, bot);
+                    
                 }, 1000);
             },
             toolClick: (ev) => {
