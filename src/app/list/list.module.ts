7b7567557567f221f8f684d7643efa4c7f22aace
Joel Zhang <rugoingdown@gmail.com>
2020-03-23 07:16:59 +0800

    angular reactive form

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
@@ -10,6 +11,7 @@ import { ListPage } from './list.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild([
       {
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'app-list',
@@ -20,7 +24,7 @@ export class ListPage implements OnInit {
         'build'
     ];
     public items: Array<{ title: string; note: string; icon: string }> = [];
-    constructor() {
+    constructor(private fb: FormBuilder) {
         for (let i = 1; i < 4; i++) {
             this.items.push({
                 title: 'Item ' + i,
@@ -28,6 +32,85 @@ export class ListPage implements OnInit {
                 icon: this.icons[Math.floor(Math.random() * this.icons.length)]
             });
         }
+
+        this.interestRates.valueChanges.pipe(debounceTime(500)).subscribe(x => {
+            console.log('interest rates value changed');
+            console.log(x);
+        });
+    }
+
+    calculatorForm = this.fb.group({
+        firstName: ['', Validators.required],
+        interestRates: this.fb.array([
+            this.fb.group({
+                periodFrom: this.fb.control(1),
+                periodTo: this.fb.control(36),
+                rate: this.fb.control(2.375),
+                payCapital: this.fb.control({
+                    value: true,
+                    disabled: true
+                })
+            })
+        ])
+    });
+
+    get interestRates() {
+        return this.calculatorForm.get('interestRates') as FormArray;
+    }
+
+    updateProfile() {
+        this.calculatorForm.patchValue({
+            firstName: 'Nancy',
+            address: {
+                street: '123 Drew Street'
+            }
+        });
+    }
+
+    changeValuesInRatesForm(index, name, type) {
+        if (name === 'periodTo') {
+            let currentValue = this.interestRates.at(index).get('periodTo').value;
+            if (type === '+') {
+                currentValue += 1;
+            } else {
+                currentValue -= 1;
+            }
+            this.interestRates.at(index).get('periodTo').setValue(currentValue);
+        } else if (name === 'rate') {
+            let currentValue = this.interestRates.at(index).get('rate').value;
+            if (type === '+') {
+                currentValue += 0.125;
+            } else {
+                currentValue -= 0.125;
+            }
+            this.interestRates.at(index).get('rate').setValue(currentValue);
+        }
+    }
+
+    addRate() {
+        // TODO- maximus rates? 10?
+        // totalMonth/10 per period
+        this.interestRates.at(this.interestRates.controls.length - 1).get('payCapital').enable();
+        this.interestRates.push(this.fb.group({
+            periodFrom: this.fb.control(37),
+            periodTo: this.fb.control(36),
+            rate: this.fb.control(2.375),
+            payCapital: this.fb.control({ value: true, disabled: true })
+        }));
+    }
+
+    deleteRate(index: number) {
+        // TODO- payCapital of last rate must be true
+        if (index === this.interestRates.controls.length - 1) {
+            this.interestRates.at(index - 1).get('payCapital').setValue(true);
+            this.interestRates.at(index - 1).get('payCapital').disable();
+        }
+
+        this.interestRates.removeAt(index);
+    }
+
+    onSubmit() {
+        console.warn(this.calculatorForm.value);
     }
 
     ngOnInit() {
